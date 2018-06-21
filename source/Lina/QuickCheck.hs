bc23ea962c49f32ec8428fe598bb5a9a8646fc3f
blakewatkins <blake.e.watkins2@gmail.com>
2018-06-21 15:13:53 -0400

    Trying to get this to accept my changes

--- source/Lina/QuickCheck.hs
+++ source/Lina/QuickCheck.hs
@@ -1,4 +1,23 @@
--- Check out:
---  http://hackage.haskell.org/package/QuickCheck-2.11.3/docs/Test-QuickCheck-Gen.html
 module Lina.QuickCheck where
 
+import Test.QuickCheck
+
+data BinTree b = EmptyTree | Node b (BinTree b) (BinTree b)
+  deriving (Show, Read, Eq)
+
+singleInsert :: a -> BinTree a
+singleInsert x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> BinTree a -> BinTree a
+treeInsert x EmptyTree = singleInsert x
+treeInsert x (Node a left right)
+  | x == a = Node x left right
+  | x < a = Node a (treeInsert x left) right
+  | x > a = Node a left (treeInsert x right)
+
+instance (Ord a, Bounded a, Random a, Num a, Arbitrary a) => Arbitrary (BinTree a) where
+  arbitrary = gen 0 100 where
+    gen :: (Ord a, Num a, Random a) => a -> a -> Gen (BinTree a)
+    gen min max | (max - min) <= 3 = return EmptryTree
+    gen min max = do
+      
