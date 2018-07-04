feb96563cee8c359574a0a3aad67aff34ccbda6e
blakewatkins <blake.e.watkins2@gmail.com>
2018-07-04 11:08:18 -0400

    BinTree Generator is working but not sizing the tree correctly.

--- source/Lina/QuickCheck.hs
+++ source/Lina/QuickCheck.hs
@@ -1,6 +1,7 @@
 module Lina.QuickCheck where
 
 import Test.QuickCheck
+import System.Random
 
 data BinTree a = EmptyTree | Node a (BinTree a) (BinTree a)
   deriving (Show, Read, Eq)
@@ -16,13 +17,14 @@ treeInsert x (Node a left right)
   | x > a = Node a left (treeInsert x right)
 
 instance Arbitrary a => Arbitrary (BinTree a) where
-  arbitrary :: Gen(BinTree a)
-  arbitrary = sized arbitrarySizedTree
+  arbitrary =
+    sized arbitrarySizedTree
 
 arbitrarySizedTree :: Arbitrary a => Int -> Gen (BinTree a)
 arbitrarySizedTree m = do
-  size <- choose(0, m)
-  case size of
-    0 -> EmptyTree
-    1 -> Node m (EmptyTree) (EmptyTree)
-    n -> Node m (arbitrarySizedTree n-1)(arbitrarySizedTree n-2)
+  t <- arbitrary
+  b <- choose (0, m `div` 2)
+  c <- choose (0, m `div` 2)
+  x <- arbitrarySizedTree b
+  y <- arbitrarySizedTree c
+  return (Node t (x)(y))
