e7cc4da9b61a536a84b11dd1503e1c93895f8446
blakewatkins <blake.e.watkins2@gmail.com>
2018-08-07 13:33:10 -0400

    Struggling with creating list of edges, can't get types to match

--- source/Lina/QuickCheck.hs
+++ source/Lina/QuickCheck.hs
@@ -1,29 +1,39 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Lina.QuickCheck where
 
+--import graphy
 import Test.QuickCheck
 import System.Random
 
 data BinTree a = EmptyTree
                | Node  a (BinTree a) (BinTree a)
-            -- | NodeB a (BinTree a) (BinTree a)
+               | NodeB a (BinTree a) (BinTree a)
   deriving (Show, Read, Eq)
 
--- showBT :: BinTree a -> String
--- showBT = undefined
+showBT :: Gen (BinTree Int) -> String
+showBT x = do
+  undefined
+  
+createGraph :: Gen (BinTree Int) -> [(Int,Int)]
+createGraph a = do
+  root <- Node root _ _
+  return root
+  
+  -- left <- Node _ y _
+  -- right <- Node _ _ z
+  -- if left == EmptyTree && right == EmptyTree then return
+  -- else if left == EmptyTree && right != EmptyTree then edgesList ++ [(root,right)]
+  --   createGraph right
+  -- else if right == EmptyTree && left != EmptyTree then edgesList ++ [(root,left)]
+  --   createGraph left
+  -- else edgesList ++ [(root,left)]
+  --   edgesList ++ [(root,right)]
+  --   createGraph left
+  --   createGraph right
 
--- instance Show (BinTree a) where
---   show = show
 
-singleInsert :: a -> BinTree a
-singleInsert x = Node x EmptyTree EmptyTree
-
-treeInsert :: (Ord a) => a -> BinTree a -> BinTree a
-treeInsert x EmptyTree = singleInsert x
-treeInsert x (Node a left right)
-  | x == a = Node x left right
-  | x < a  = Node a (treeInsert x left) right
-  | x > a  = Node a left (treeInsert x right)
+--instance Show (BinTree a) where
+  --show = showBT
 
 instance Arbitrary (BinTree Int) where
   arbitrary = sized arbitrarySizedTree 
@@ -32,6 +42,9 @@ arbitrarySizedTree :: Int -> Gen (BinTree Int)
 arbitrarySizedTree 0 = return EmptyTree
 arbitrarySizedTree 1 = do
   d <- arbitrary :: Gen Int
+  nodePick <- choose (0,1)  :: Gen Int
+  if nodePick == 0 then return $ Node d EmptyTree EmptyTree
+  else return $ NodeB d EmptyTree EmptyTree
   return $ Node d EmptyTree EmptyTree
 arbitrarySizedTree m = ranNode Node m
 
